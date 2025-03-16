1e15ab0ce6b4073a45e021f1bab9496ad2021da0
unknown <ashblazer17@gmail.com>
2025-03-16 11:37:02 +0500

    fully updated

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -6,7 +6,7 @@ const PORT = 5000;
 app.use (cors());
 
 app.get('/',(req,res)=>{
-    res.send(`Hello From Node.js Backend!!`)
+    res.send(`Hello From Node.js!! Backend!!`)
 });
 app.listen(PORT,()=>{
     console.log(`Server is runnning on http://localhost:${PORT}`)
