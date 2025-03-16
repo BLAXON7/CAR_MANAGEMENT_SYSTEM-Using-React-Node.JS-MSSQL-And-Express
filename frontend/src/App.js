d43baa28aecee26316aa8bcb18a8f52070ae6282
unknown <ashblazer17@gmail.com>
2025-03-16 11:26:47 +0500

    fully updated

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+
+
+function App() {
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        fetch('http://localhost:5000')
+            .then((response) => response.text())
+            .then((data) => setMessage(data));
+    }, []);
+
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>React Frontend</h1>
+            <p>{message}</p>
+        </div>
+    );
+}
+
+export default App;
