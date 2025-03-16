c6a1aa72e80283c5a839d88b25bfefbde24e456e
unknown <ashblazer17@gmail.com>
2025-03-16 11:29:06 +0500

    Ignoring node_modules

---
+++ BACKEND/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors')
+const app =  express();
+const PORT = 5000;
+
+app.use (cors());
+
+app.get('/',(req,res)=>{
+    res.send(`Hello From Node.js Backend!!`)
+});
+app.listen(PORT,()=>{
+    console.log(`Server is runnning on http://localhost:${PORT}`)
+});
