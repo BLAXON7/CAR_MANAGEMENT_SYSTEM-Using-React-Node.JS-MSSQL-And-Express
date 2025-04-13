d64146a5b00269ed5cdb459baa5dceddbb90b353
unknown <ashblazer17@gmail.com>
2025-04-13 15:37:11 +0500

    fully updated

---
+++ BACKEND/DB_config.js
@@ -0,0 +1,28 @@
+// dbconfig.js
+const sql = require('mssql');
+require('dotenv').config();
+
+const dbConfig = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,  // e.g., localhost or your server address
+    database: process.env.DB_NAME,
+    options: {
+        encrypt: true,  // Use encryption
+        trustServerCertificate: true // Change to true for local development
+    }
+};
+
+// Function to connect to the database
+const connectToDB = async () => {
+    try {
+        const pool= await sql.connect(dbConfig);
+        console.log('Connected to the database');
+        return pool;
+    } catch (err) {
+        console.error('Database connection error: ', err.message);
+    }
+};
+
+
+module.exports = { sql, connectToDB };
--- BACKEND/Model/CarModel.js
+++ BACKEND/Model/CarModel.js
@@ -1,17 +1,16 @@
-const { sql , connectToDB } = require('../config/DB_config');  // Correct relative path to DB_config.js
+const { sql , connectToDB } = require('../DB_config');  // Correct relative path to DB_config.js
 
-// const getUser = async () => {
-//   try {
-//     // Ensure the connection is established before making any queries
-//     const pool = await connectToDB();  // Await the connection
-//     const result = await pool.request().query('SELECT * FROM SELLER');
-//     return result.recordset;  // Return query results
-//   } catch (err) {
-//     console.error('Error executing query:', err);
-//     throw err;  // Rethrow error for further handling
-//   }
-// };
+const getUser = async () => {
+  try {
+    // Ensure the connection is established before making any queries
+    const pool = await connectToDB();  // Await the connection
+    const result = await pool.request().query('SELECT * FROM [[USER]]]');
+    return result.recordset;  // Return query results
+  } catch (err) {
+    console.error('Error executing query:', err);
+    throw err;  // Rethrow error for further handling
+  }
+};
 
-console.log("DB Config in CarModel:", { connectToDB, sql });  // Log the imported config
 
-// module.exports = { getUser };
+ module.exports = { getUser };
