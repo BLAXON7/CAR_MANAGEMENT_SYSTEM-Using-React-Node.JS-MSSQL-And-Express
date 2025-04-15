a863d46450b762b477f77dc05181dd00a1c294a3
baber ali <baberalimunir@gmail.com>
2025-04-15 10:36:19 +0500

    3

--- BACKEND/Model/CarModel.js
+++ BACKEND/Model/CarModel.js
@@ -1,11 +1,15 @@
 const { sql , connectToDB } = require('../DB_config');  // Correct relative path to DB_config.js
 
-const getUser = async () => {
+const getUser = async (Rentalid) => {
   try {
     // Ensure the connection is established before making any queries
     const pool = await connectToDB();  // Await the connection
-    const result = await pool.request().query('SELECT * FROM [[USER]]]');
+    const result = await pool.request()
+    .input('Rentalid',sql.Int,Rentalid)
+    .execute('ReturnCar');  // Execute the query
+    
     return result.recordset;  // Return query results
+
   } catch (err) {
     console.error('Error executing query:', err);
     throw err;  // Rethrow error for further handling
