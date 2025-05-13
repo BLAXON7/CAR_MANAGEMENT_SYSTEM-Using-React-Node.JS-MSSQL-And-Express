d761543a1596c40fb713cd87fb88b940c7017ec1
ezaan-ullah <m.ezaanullah@gmail.com>
2025-05-13 20:35:07 +0500

    Add files via upload

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -3,7 +3,10 @@ const cors = require('cors')
 const app =  express();
 const PORT = 5000;
 const userroutes = require('./Routes/CarRoutes');
-
+const MarketplaceRouter = require('./Routes/MarketPlaceRoute');
+const MyPurchasesRouter = require('./Routes/MyPurchasesRoute');
+const MyRentalsRouter = require('./Routes/MyRentalsRoute');
+const TransactionsRouter = require('./Routes/TransactionsRoute');
 app.use(express.json()); 
 app.use(cors());
 app.use('/api', userroutes);
@@ -12,6 +15,23 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ error: 'Something went wrong!' });
   });
+
+// // // // // Edited Portion Start // // // // //
+
+// Marketplace Route
+app.use('/api/marketplace', MarketplaceRouter);
+
+// My Purchases Route
+app.use('/api/my-purchases', MyPurchasesRouter);
+
+// My Rentals Route
+app.use('/api/my-rentals', MyRentalsRouter);
+
+// Transactions Route
+app.use('/api/transactions', TransactionsRouter);
+
+// // // // // Edited Portion End // // // // //
+
 
 app.get('/',(req,res)=>{
     res.send(`Hello From Node.js!! Backend!!`)
